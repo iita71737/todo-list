e92ecacc09248bc6d30d93a18a785d66c11f3b48
danlin <iita71737@gmail.com>
2021-06-04 00:46:22 +0800

    feat: add bootswatch style

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const methodOverride = require('method-override')
 const express = require('express')
-const app = express()
 const port = 3000
-const Todo = require('./models/todo') 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true , useUnifiedTopology: true })
-const db = mongoose.connection
 
+const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
+const app = express()
+require('./config/mongoose')
+
 app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
 
@@ -16,68 +15,10 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
-db.on('error', () => {
-    console.log('mongodb error')
-})
-db.once('open', () => {
-    console.log('mongodb connected')
-})
 
-app.get('/', (req, res) => {
-    Todo.find() // 取出 Todo model 裡的所有資料
-    .sort({ name : 'desc'})
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
+
+app.use(routes)
 
 app.listen(port , () => {
     console.log(`app is running on http://localhost:${port}`)
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      return todo.save()
-    })
-    .then(()=> res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
 })
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,6 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true , useUnifiedTopology: true })
-const db = mongoose.connection
+require('../../config/mongoose')
 
-db.on('error', () => {
-    console.log('mongodb error')
-})
 db.once('open', () => {
     console.log('mongodb connected')
     for (let i = 0; i < 10; i++) {
